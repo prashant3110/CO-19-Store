ba39c95b34338e8c67d10b334d1320c945c669c3
Prashant <53570660+prashant3110@users.noreply.github.com>
2021-07-12 17:37:01 +0530

    Add files via upload

---
+++ CO-19 Store/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CO-19 Store project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CO-19 Store.settings')
+
+application = get_asgi_application()
